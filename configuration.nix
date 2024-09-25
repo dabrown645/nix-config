94d74f29365e1915246360e9506a5771bc735bc0
David A Brown <dabrown645@gmail.com>
2024-09-24 19:12:38 -0700

    🐞 FIX: fixed homemanager in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,13 +2,12 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs,... }:
+{ config, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
     ];
 
   # Allow experimental commands
